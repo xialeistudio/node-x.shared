371729efb40ae277b3bd19a5b761482bb76ec244
xialeistudio <1065890063@qq.com>
2017-02-15 16:56:31 +0800

    lib/amqp

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * @date 2017/2/15
+ * @author xialeistudio<1065890063@qq.com>
+ */
+'use strict';
+module.exports = {
+  amqp: require('./lib/amqp')
+};
---
+++ test/amqp.js
@@ -0,0 +1,36 @@
+/**
+ * @date 2017/2/15
+ * @author xialeistudio<1065890063@qq.com>
+ */
+var AMQP = require('../lib/amqp');
+var config = require('./amqp.config');
+var mocha = require('mocha');
+var assert = require('assert');
+var describe = mocha.describe;
+var it = mocha.it;
+
+describe('test lib/amqp.js', function () {
+  this.timeout(10000);
+  var now = Date.now();
+  it('test publish', function (done) {
+    var amqp = new AMQP(config.connection);
+    amqp.setPublishConfig(config.exchangeName, config.routingKey);
+    amqp.publish(now.toString()).then(function (result) {
+      assert(result);
+      done();
+    }).catch(done);
+  });
+  it('test consumer', function (done) {
+    var amqp = new AMQP(config.connection);
+    amqp.setQueueConfig(config.queueName, config.queueOptions);
+    amqp.subscribe(function (e, msg, channel) {
+      if (e !== null) {
+        done(e);
+        return;
+      }
+      channel.ack(msg);
+      assert(msg.content.toString() === now.toString());
+      done();
+    });
+  });
+});
